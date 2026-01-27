41cc04a09bf8f4bb2010e0f124fd4623ef07a0e3
alexa <alexey3803@gmail.com>
2026-01-27 14:59:56 +0300

    web сайт типо

--- web.py
+++ web.py
@@ -2,5 +2,13 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/index')
+@app.route('/')
+def index():
+    return 'Hello World!'
+
+@app.route('/about')
+def about():
+    return 'about page!'
 if __name__ == '__main__':
     app.run(debug=True)
