89d06eb2fa3dfd9a2b982b029704c3ac75abf5dc
alexa <alexey3803@gmail.com>
2026-01-27 15:23:44 +0300

    web сайт типо

--- web.py
+++ web.py
@@ -1,14 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/index')
 @app.route('/')
 def index():
-    return 'Hello World!'
+    return render_template('index.html')
 
 @app.route('/about')
 def about():
-    return 'about page!'
+    return render_template('about.html')
 if __name__ == '__main__':
     app.run(debug=True)
